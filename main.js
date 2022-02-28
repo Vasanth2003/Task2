341c4157b247f3b510d01579a6467ae0290ddfb2
Vasanth <87744906+Vasanth2003@users.noreply.github.com>
2022-02-28 18:26:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+    function numvis() {
+      document.getElementById("n").innerHTML = "Mob No.";
+      document.getElementById("n").style.fontSize = "2rem";
+    }
+
+    function phvis() {
+        document.getElementById("n").innerHTML = "Contact us for Orders!!";
+        document.getElementById("n").style.fontSize = "1.5rem";
+      }
+
